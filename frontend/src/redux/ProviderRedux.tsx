a8e8a85949cd81693fe77f313ed11699e25ff441
smtamnay <smtamay@ixmatix.com>
2025-07-18 11:46:33 -0600

    ✨ feat: add useGetCurrentUserQuery import to ProviderRedux for user data retrieval

--- frontend/src/redux/ProviderRedux.tsx
+++ frontend/src/redux/ProviderRedux.tsx
@@ -2,6 +2,7 @@
 import React, { FC, PropsWithChildren } from "react";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { useGetCurrentUserQuery } from "@/api/frontendApi";
 
 const ProviderRedux: FC <PropsWithChildren> = ({children}) => {
   return <Provider store={store}>{children}</Provider>;
